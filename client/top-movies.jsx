4481813948525291534c4b47b57de3bd81f9b71e
Alvin Cheng <ascheng21@gmail.com>
2017-08-15 13:14:43 -0700

    Added TopMovies component. #11

---
+++ client/top-movies.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class TopMovies extends React.Component {
+  render() {
+    return (
+      <section className="container-fluid">
+        <div className="row">
+          <div className="col-sm-12">
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
